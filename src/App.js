305f9b88848b94cc7c300c9a087aae9bdda53ccf
jamaosve <jamaosve@student.jyu.fi>
2019-04-20 20:52:31 +0300

    formit koossa

--- src/App.js
+++ src/App.js
@@ -7,16 +7,51 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-           Ladataan sovellusta...
+          Kuinka pitkä on yksi kullinluikaus?    
           </p>
+          <form>
+          <p>
+          <label>
+           Kokonaismatka kilometreinä:  
+          <input type="text" name="km" />
+          </label>
+          </p>
+          <p>
+          <label>
+           Matkustusnopeus:  
+          <input type="text" name="speed" />
+          </label>
+          </p>
+          <p>
+          <label>
+           Ikä:  
+          <input type="text" name="age" />
+          </label>
+          </p>
+          <p>
+          <label>
+           Kullin pituus:  
+          <input type="text" name="dick" />
+          </label>
+          </p>
+          <p>
+          Matkustusväline:        
+          <select>
+            <option value="walk">Apostoli</option>
+            <option value="car">Auto</option>
+            <option selected value="bus">Bussi</option>
+            <option value="train">Juna</option>
+            <option value="plane">Lentskari</option>
+</select>
+          </p>
+          
+  <input type="submit" value="Submit" />
+</form>
         </header>
       </div>
     );
   }
 }
-
-
 
 export default App;
