04c7e17db5773f0ebf150318da18fd1b13579711
jamaosve <jamaosve@student.jyu.fi>
2019-04-20 19:55:03 +0300

    paskaa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './200.gif';
 import './App.css';
 
 class App extends Component {
@@ -9,20 +9,14 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           Ladataan sovellusta...
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
   }
 }
+
+
 
 export default App;
